f99818b9b7802c4fec17aafe548aa86a39471976
pochs <ochs.pascal@gmail.com>
2021-10-07 16:50:32 +0100

    integrate news reports

--- crypto/views.py
+++ crypto/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from crypto.config_file import api_key
 
+# API and data from https://www.cryptocompare.com/
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json 
+    api_request = requests.get(f"https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api': api})
