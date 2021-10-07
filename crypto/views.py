350671e72c2ff78633d380c0bc84bde0b8de9392
pochs <ochs.pascal@gmail.com>
2021-10-07 20:33:32 +0100

    App with table

--- crypto/views.py
+++ crypto/views.py
@@ -5,6 +5,18 @@ from crypto.config_file import api_key
 def home(request):
     import requests
     import json 
-    api_request = requests.get(f"https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    # Grab Crypto Price Data
+    #
+    price_request = requests.get(f"https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,SHIB,DOT,SOL,ADA,DODGE,FTM,XRP,BUSD&tsyms=GBP&api_key={api_key}")
+    price = json.loads(price_request.content)
+    
+    # Grab Crypto News
+    api_request = requests.get(f"https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key={api_key}")
     api = json.loads(api_request.content)
-    return render(request, 'home.html', {'api': api})
+    return render(request, 'home.html', {
+        'api': api,
+        'price': price
+        })
+
+def prices(request):
+    return render(request, 'prices.html', {})
