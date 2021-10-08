c30c14f00b52d853ebc61df5905bae6150514244
pochs <ochs.pascal@gmail.com>
2021-10-08 16:19:13 +0100

    complete webisite

--- crypto/views.py
+++ crypto/views.py
@@ -5,8 +5,8 @@ from crypto.config_file import api_key
 def home(request):
     import requests
     import json 
+
     # Grab Crypto Price Data
-    #
     price_request = requests.get(f"https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,SHIB,DOT,SOL,ADA,DODGE,FTM,XRP,BUSD&tsyms=GBP&api_key={api_key}")
     price = json.loads(price_request.content)
     
@@ -19,4 +19,21 @@ def home(request):
         })
 
 def prices(request):
-    return render(request, 'prices.html', {})
+    import requests
+    import json 
+    if request.method == 'POST':
+        quote = request.POST['quote']
+        quote = quote.upper()
+
+        # Grab Crypto Price Data
+        crypto_request = requests.get(f"https://min-api.cryptocompare.com/data/pricemultifull?fsyms={quote}&tsyms=GBP&api_key={api_key}")
+        crypto = json.loads(crypto_request.content)
+
+        return render(request, 'prices.html', {
+            'quote': quote,
+            'crypto': crypto
+            })
+    else:
+        notfound = "Please enter the crypto currence into the 'Lookup Crypto' field to search for the latest price. \nPlease use the short-name tags. \nIf you want to search for multiple crypto currency type them seperated by a comma."
+        return render(request, 'prices.html', {'notfound': notfound})
+
